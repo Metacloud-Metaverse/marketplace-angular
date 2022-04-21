85655cc877399d2b99e2119678d0ede7f1f96c2b
Axel Camiletti <45402442+axelcamiletti@users.noreply.github.com>
2022-04-21 10:48:52 -0300

    Marketplace Main Page 80% + General styles 50%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'metacloud-marketplace-angular';
+  title = 'Welcome to the metacloud metaverse | metacloud';
 }
