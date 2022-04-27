2d941e4d53b9f48ffec8c11f2e2ccf67c5db919f
Matias Camiletti <matias.camiletti@gmail.com>
2022-04-27 12:32:24 -0300

    Add entities.

---
+++ src/app/entities/land.ts
@@ -0,0 +1,12 @@
+export class Land {
+    id: number = 0;
+    coord_x: number = 0;
+    coord_y: number = 0;
+    user_id: number = 0;
+    is_state_use: number = 0;
+    is_on_sale: number = 0;
+    is_empty: number = 0;
+    price: number = 0;
+    created_at?: string;
+    updated_at?: string;
+}
