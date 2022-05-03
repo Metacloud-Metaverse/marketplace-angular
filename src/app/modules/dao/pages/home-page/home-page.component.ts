fb2ffabd3fdbce1697c51e48c2c6536fca4bb431
Axel Camiletti <45402442+axelcamiletti@users.noreply.github.com>
2022-05-03 19:32:48 -0300

    Dao Home page 100%

--- src/app/modules/dao/pages/home-page/home-page.component.ts
+++ src/app/modules/dao/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private titleService: Title,
+    private metaTagService: Meta,
+  ) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle('DAO | metacloud');
+    this.metaTagService.updateTag(
+      { name: 'description', content: '' }
+    );
   }
 
 }
