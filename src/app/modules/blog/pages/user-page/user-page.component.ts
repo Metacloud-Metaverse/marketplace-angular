c8fccab00ca7d7c5c10e196d669c6b2fb646ba09
Axel Camiletti <catriel.camiletti@gmail.com>
2022-05-04 14:34:16 -0300

    User Page 100%

--- src/app/modules/blog/pages/user-page/user-page.component.ts
+++ src/app/modules/blog/pages/user-page/user-page.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { ComponentAnimation } from 'src/app/modules/mc-core/animations/component.animation';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.scss']
+  styleUrls: ['./user-page.component.scss'],
+  animations: [ComponentAnimation.componentAnimation]
 })
 export class UserPageComponent implements OnInit {
 
-  constructor() { }
+  articles:any = [
+    {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
+  ]
+  isLoading = false;
+  slice = 10;
+
+  constructor(
+    private titleService: Title,
+    private metaTagService: Meta,
+  ) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle('[User_name] | metacloud');
+    this.metaTagService.updateTag(
+      { name: 'description', content: '' }
+    );
   }
 
+  loadMoreComments(){
+    this.slice = this.slice + 10;
+  }
 }
