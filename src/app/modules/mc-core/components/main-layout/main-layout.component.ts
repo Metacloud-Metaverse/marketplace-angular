0b7ce9bafafc291fffb49afd52dae033502df758
Matias Camiletti <matias.camiletti@gmail.com>
2022-06-07 10:54:26 -0300

    Install web3modal.

--- src/app/modules/mc-core/components/main-layout/main-layout.component.ts
+++ src/app/modules/mc-core/components/main-layout/main-layout.component.ts
@@ -6,6 +6,12 @@ import { animate, style, transition, trigger } from '@angular/animations';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { LoginModalComponent } from '../../modals/login-modal/login-modal.component';
 
+import Web3 from "web3";
+
+declare const window: any;
+const Web3Modal = window.Web3Modal.default;
+//import Web3Modal from "web3modal";
+
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
@@ -54,10 +60,66 @@ export class MainLayoutComponent implements OnInit {
     this.isMenuOpen = false;
   }
 
-  openLoginModal(){
-    this.dialog.open(LoginModalComponent, {
-      width: '520px',
-      /* panelClass: 'modal-full-width-mobile', */
+  async openLoginModal(){
+    const providerOptions = {
+      // See Provider Options Section
+      /*binancechainwallet: {
+        package: true
+      },*/
+    };
+
+    const web3Modal = new Web3Modal({
+      //network: "mainnet", // optional
+      network: "mainnet", // optional
+      cacheProvider: true, // optional
+      providerOptions // required
     });
+
+    const provider = await web3Modal.connect();
+
+    // Subscribe to accounts change
+    provider.on("accountsChanged", (accounts: string[]) => {
+      console.log(accounts);
+    });
+
+    // Subscribe to chainId change
+    provider.on("chainChanged", (chainId: number) => {
+      console.log('Chain Changed: HEX: ' + chainId + ' - String: ' + this.hex2a(chainId));
+      console.log(chainId);
+    });
+
+    // Subscribe to provider connection
+    provider.on("connect", (info: { chainId: number }) => {
+      console.log(info);
+    });
+
+    // Subscribe to provider disconnection
+    provider.on("disconnect", (error: { code: number; message: string }) => {
+      console.log(error);
+    });
+
+    const web3 = new Web3(provider);
+    
+    web3.eth.getAccounts().then(data => console.log(data));
+
+    /*this.dialog.open(LoginModalComponent, {
+      width: '520px',
+    });*/
+  }
+
+  private toHex(stringToConvert: string) {
+    return stringToConvert
+      .split('')
+      .map((c) => c.charCodeAt(0).toString(16).padStart(2, '0'))
+      .join('');
+  }
+
+  hex2a(hexx: any) {
+    let hexString = hexx;
+    let strOut = '';
+        for (let x = 0; x < hexString.length; x += 2) {
+            strOut += String.fromCharCode(parseInt(hexString.substr(x, 2), 16));
+        }
+    return strOut;
   }
 }
